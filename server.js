d3c13e1b44c70a7832930f657a1815f54f67397d
Shakya7 <shakyasarkar72@gmail.com>
2022-05-21 09:16:19 +0530

    BACKEND deployed --> HEROKU with ENV var

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const mongoose=require("mongoose");
 
 function connectToDB(){
     try{
-    //mongoose.connect(process.env.DB_CONNECTION.replace("<password>",process.env.DB_PASSWORD).replace("myFirstDatabase",process.env.DB_NAME))
-    mongoose.connect("mongodb+srv://shakya:shakyaDev@cluster0.3hung.mongodb.net/FlipKart_Clone_MERN?retryWrites=true&w=majority",
+    mongoose.connect(process.env.DB_CONNECTION.replace("<password>",process.env.DB_PASSWORD).replace("myFirstDatabase",process.env.DB_NAME),
     {
         useNewUrlParser: true
     })
+    /*mongoose.connect("mongodb+srv://shakya:shakyaDev@cluster0.3hung.mongodb.net/FlipKart_Clone_MERN?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true
+    })*/
     console.log("Connected to Database successfully")
     }catch(err){
         console.log(err);
